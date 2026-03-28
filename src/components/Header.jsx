7f38259946e151da716d51ee293c333f3a736329
meaghanravagnati <mravagnati@frba.utn.edu.ar>
2026-03-28 20:54:38 -0300

    add: logo and links in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 const Header = () => {
     return(
-        <section>
-            <header></header>
-        </section>
+            <header>
+                <img src="./src/assets/logo.jpg" alt="" />
+                <nav>
+                    <a href="">About us</a>
+                    <a href="">Blog</a>
+                    <a href="">Dates</a>
+                    <a href="">Contact</a>
+                </nav>
+            </header>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Header } from './components/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header/>
   </StrictMode>,
 )
