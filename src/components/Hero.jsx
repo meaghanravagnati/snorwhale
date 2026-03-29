aff63718a3a2bd5bc6600c232e2e311a0dc13aff
meaghanravagnati <mravagnati@frba.utn.edu.ar>
2026-03-28 21:33:13 -0300

    add: Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+    return(
+        <section className="hero-page">
+            <h1>SnorWhale</h1>
+            <p>Swim with giants in Norway. An unforgettable experience.</p>
+            <p>Exclusive Groups of Up to 10.</p>
+
+            <a href="">Upcoming departures</a>
+
+        </section>
+    )
+}
+
+export { Hero }
