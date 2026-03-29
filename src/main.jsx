0643afae9766fee2a7b067c67dc2d5e8210b5001
meaghanravagnati <mravagnati@frba.utn.edu.ar>
2026-03-28 21:33:38 -0300

    add: import Hero

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Header } from './components/Header.jsx'
+import { Hero } from './components/Hero.jsx'
 
 import "./styles/main.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
+    <Hero/>
   </StrictMode>,
 )
