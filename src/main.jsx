088a20e5e9d4114bf107c68351aa7a3b53ce7d23
meaghanravagnati <mravagnati@frba.utn.edu.ar>
2026-04-03 22:30:26 -0300

    add: images for a slider and a section in main with important info

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Header } from './components/Header.jsx'
 import { Hero } from './components/Hero.jsx'
+import { Main } from './components/Main.jsx'
 
 import "./styles/main.css"
 
@@ -9,5 +10,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
     <Hero/>
+    <Main/>
   </StrictMode>,
 )
